9a470489207a8e32c9a8c4e8b06aa7b3183d936e
shapiraoz <shapiraoz@b4e4f271-b3f5-8df1-f3a1-4eb18db89f48>
2010-10-23 15:41:57 +0000

    add basic SQL Skeleton

---
+++ Shared/DataLowLevelCore/src/Classes/ASQLConnection.java
@@ -0,0 +1,31 @@
+package Classes;
+
+
+public class ASQLConnection {
+	
+	protected String m_username;
+	protected String m_password;
+	protected String m_connectionString;
+	
+	public String getM_username() {
+		return m_username;
+	}
+	public void setM_username(String m_username) {
+		this.m_username = m_username;
+	}
+	public String getM_password() {
+		return m_password;
+	}
+	public void setM_password(String m_password) {
+		this.m_password = m_password;
+	}
+	public String getM_connectionString() {
+		return m_connectionString;
+	}
+	public void setM_connectionString(String string) {
+		m_connectionString = string;
+	}
+	
+
+	
+}
---
+++ Shared/DataLowLevelCore/src/Interfaces/ISQLConnection.java
@@ -0,0 +1,19 @@
+package Interfaces;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+
+public interface ISQLConnection {
+
+	Connection GetConnection();
+	void Connect();
+	void Connect(String connectionString,String userName,String Password);
+	void Connect(String userName,String Password);
+	void Reconnect(String userName,String Password);
+	
+	void SetDriver(String driver);
+	void LoadDriver(String driver);
+	ResultSet ExcuteQurey(String sqlQurey);
+	ResultSet ExcuteStoredProcedures(String procdureName);
+	
+}
