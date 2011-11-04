865d1e8f6fc068b893cf5056964935f96738a8e8
shapiraoz@gmail.com <shapiraoz@gmail.com@b4e4f271-b3f5-8df1-f3a1-4eb18db89f48>
2011-11-04 17:32:01 +0000

    add Taklito

---
+++ trunk/School/classes/practice/Taklito/TaklitoData/IManageGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TaklitoData
+{
+    public interface IManageGroup
+    {
+        void AddGroup(string name);
+        List<Group> getAllGroups();
+
+        
+    }
+}
