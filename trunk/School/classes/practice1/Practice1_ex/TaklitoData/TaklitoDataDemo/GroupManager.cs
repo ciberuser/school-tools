55814f331c47e005c620d26b5439c30aa8a3a3cf
shapiraoz@gmail.com <shapiraoz@gmail.com@b4e4f271-b3f5-8df1-f3a1-4eb18db89f48>
2011-11-07 21:54:54 +0000

    add practice

---
+++ trunk/School/classes/practice1/Practice1_ex/TaklitoData/TaklitoDataDemo/GroupManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TaklitoDataDemo
+{
+    public class GroupManager:IGroupManager
+    {
+
+
+
+
+        #region IGroupManager Members
+
+        public void AddGroup(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group[] GetAllGroups()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveGroup(int groupId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveGroup(string GroupName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetGroupId(string GroupName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void JoinArtistToGroup(Artist artist, ref Group group)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void JoinArtistToGroup(Artist artist, int groupId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveArtistFromGroupToGroup(Artist artist, int groupFrom, int groupTo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveArtistFromGroup(Artist artistId, int groupId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Group FindGroup(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
