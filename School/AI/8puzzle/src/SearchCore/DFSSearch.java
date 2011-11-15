aebd73655b7e40b587d58d6498a8d59976c51406
shapiraoz@gmail.com <shapiraoz@gmail.com@b4e4f271-b3f5-8df1-f3a1-4eb18db89f48>
2011-11-15 15:41:58 +0000

    add java version

---
+++ School/AI/8puzzle/src/SearchCore/DFSSearch.java
@@ -0,0 +1,15 @@
+package SearchCore;
+
+public class DFSSearch extends ASearch implements ISearch {
+
+		
+	public DFSSearch() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public void Search()
+	{
+		
+	}
+}
