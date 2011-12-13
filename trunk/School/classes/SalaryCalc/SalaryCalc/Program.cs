e6e052a321c3bd0a867e82fcecab4b530b75488f
shapiraoz@gmail.com <shapiraoz@gmail.com@b4e4f271-b3f5-8df1-f3a1-4eb18db89f48>
2011-12-13 11:55:49 +0000

    finish practice 3

--- trunk/School/classes/SalaryCalc/SalaryCalc/Program.cs
+++ trunk/School/classes/SalaryCalc/SalaryCalc/Program.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.InteropServices;
 
 namespace SalaryCalc
 {
     class Program
     {
+        [DllImport("msvcrt")]
+        static extern int _getch();
+
         static void Main(string[] args)
         {
             ISalaryCalculator salaryCalculator = new SalaryCalculator();
@@ -14,10 +18,22 @@ namespace SalaryCalc
             Console.WriteLine("insert first name and then last name for getting salary details");
             string name = Console.ReadLine();
             string last = Console.ReadLine();
-            Console.WriteLine("your salary is :{0}", salaryCalculator.GetSalary(name, last));
-            Console.WriteLine("your worked {0} hours", salaryCalculator.GetWorkerHour(name, last));
-            Console.WriteLine("thanks!!");
-            Console.ReadLine();
+            int sal = salaryCalculator.GetSalary(name, last);
+            if (sal == 0) 
+            {
+                Console.WriteLine("worker didn't found!!");
+            }
+            else
+            {
+                Console.WriteLine("your salary is :{0}", sal);
+                Console.WriteLine("your worked {0} hours", salaryCalculator.GetWorkerHour(name, last));
+                Console.WriteLine("thanks!!");
+            }
+            Console.WriteLine("Press any key to end..");
+            _getch(); // this works only when import dll 
+        
+           
+            
             
 
         }
