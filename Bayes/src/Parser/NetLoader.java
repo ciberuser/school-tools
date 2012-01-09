5d5a24eab6b7c12b2f171ca5fa6ed9a560745274
shapiraoz <shapiraoz@b4e4f271-b3f5-8df1-f3a1-4eb18db89f48>
2012-01-09 20:48:09 +0000

    fix the algorithm

--- Bayes/src/Parser/NetLoader.java
+++ Bayes/src/Parser/NetLoader.java
@@ -8,12 +8,16 @@ import java.util.regex.Pattern;
 public class NetLoader extends BasePareser
 {
 	static final String LOAD_PREFIX = "[Ll][oO][Aa][Dd]";
-	static final String VARIABLE =  "variable[ ]+[A-Za-z]";
+	static final String VARIABLE =  "variable[ ]+[A-Za-z]+";
 	static final String VAR = "variable";
-	static final String PRO =  "probability";
+	static final String PRO =  "probability[ ]+[A-Za-z]+[ ]|[ ][A-Za-z ]+[\r\n][ ]+";
+	static final String PRIO = "[ft][ ][0-9.]{3,5}";
 	
-	Pattern m_pattern;
-	Matcher m_matcher;
+	Pattern m_patternVar;
+	Pattern m_patternPrio;
+	
+	Matcher m_matcherVar;
+	Matcher m_matcherPrio;
 	Scanner m_FileScanner;
 	String m_netString ;
 	Network m_network; 
@@ -26,19 +30,69 @@ public class NetLoader extends BasePareser
 	    CreateNetwork();
 	}
 	
-	private void CreateNetwork()
+	private void AddNode(String line)
 	{
-		if (m_netString == null)return ;
-		m_pattern = Pattern.compile(VARIABLE);
-		m_matcher = m_pattern.matcher(m_netString);
-	    while(m_matcher.find())
+		m_patternVar = Pattern.compile(VARIABLE);
+		m_matcherVar = m_patternVar.matcher(line);
+		while(m_matcherVar.find())
 	    {
-	    	String s = m_matcher.group(0);
+	    	String s = m_matcherVar.group(0);
 	    	s=s.replace(VAR, " ");
 	    	System.out.println(s.trim());
 	    }
+		
 	}
 	
+	private void AddProbability(String line )
+	{
+		m_patternPrio = Pattern.compile(PRO);
+		m_matcherPrio = m_patternPrio.matcher(line);
+		while (m_matcherPrio.find())
+		{
+			String s = m_matcherPrio.group(0);
+			s.replace("probability", "");
+			String[] sArr = s.split("|");
+			
+		}
+	}
+	
+	private void CreateNetwork()
+	{
+		if (m_netString == null)return ;
+		
+		
+		while(m_matcherVar.find())
+	    {
+	    	String s = m_matcherVar.group(0);
+	    	s=s.replace(VAR, " ");
+	    	System.out.println(s.trim());
+	    }
+				
+		
+	}
+	
+	void AddPrio(String nodeName, String[] parents )
+	{
+		String str;
+        
+		BufferedReader reader = new BufferedReader(
+		  new StringReader(nodeName));
+		        
+		try 
+		{
+		  while ((str = reader.readLine()) != null) 
+		  {
+		        if (str.length() > 0) 
+		        {
+		        		AddNode(str);
+	        	}
+		  }
+		} 
+		catch(IOException e)
+		{
+		  e.printStackTrace();
+		}
+	}
 	
 	
 	public Network GetNetwork()
