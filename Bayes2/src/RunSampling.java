0d83a56de3b7e94654c2d9d174115ad4765fcb18
shapiraoz <shapiraoz@b4e4f271-b3f5-8df1-f3a1-4eb18db89f48>
2012-01-09 23:04:57 +0000

    finish !!!

--- Bayes2/src/RunSampling.java
+++ Bayes2/src/RunSampling.java
@@ -14,7 +14,7 @@ public class RunSampling {
     public static void main(String argv[])
 	throws FileNotFoundException, IOException {
 
-	int max_iters = 0;
+	int numSamples = 0;
 	
 	String bn_filename = null;
 	String query_filename = null;
@@ -22,7 +22,7 @@ public class RunSampling {
 	try {
 	    bn_filename = argv[0];
 	    query_filename = argv[1];
-	    max_iters = Integer.parseInt(argv[2]);
+	    numSamples = Integer.parseInt(argv[2]);
 	   // freq = Integer.parseInt(argv[3]);
 	} catch(Exception e) {
 	    System.err.println("Arguments: <bn_filename> <query_filename> <max_iterations> <printing_freq>");
@@ -35,11 +35,12 @@ public class RunSampling {
 
 	Sampling sample = new Sampling(bn, q);
 
-	NumberFormat nf = new DecimalFormat("0.000000");
-	System.out.println("runnig sampling file "+bn_filename);
+	System.out.println("-------------------------------------");
+	System.out.print("runnig sampling file "+bn_filename);
 	System.out.print(" with query file "+ query_filename);
-	System.out.print("the sampling result :");
-	System.out.print(nf.format( sample.runMoreIterations(max_iters) ));
+	System.out.print("\nnumber of samples : ");
+	System.out.print(numSamples);
+	sample.runMoreIterations(numSamples);
 	
 
     }
