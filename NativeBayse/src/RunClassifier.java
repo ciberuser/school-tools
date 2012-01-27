e18438bbe878d6c72440acf4950af89be9b632d3
shapiraoz <shapiraoz@b4e4f271-b3f5-8df1-f3a1-4eb18db89f48>
2012-01-27 18:28:19 +0000

    Start Building Classifier

--- NativeBayse/src/RunClassifier.java
+++ NativeBayse/src/RunClassifier.java
@@ -8,6 +8,16 @@ public class RunClassifier {
 		// TODO Auto-generated method stub
 		System.out.println("insert file with word list , data and lables");
 		
+		String WordsFile = args[0];
+		//String AritcleListFile =  args[1] ;
+		
+		//String ClassFile =  args[2] ;
+		
+		Classifier classifier = new Classifier();
+		
+		classifier.AddWordList(WordsFile);
+		
+		
 		
 	}
 
