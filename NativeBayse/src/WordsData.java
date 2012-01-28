1ef9c7e84c2615bb87a53032cd74b1e9e38a5e5d
shapiraoz <shapiraoz@b4e4f271-b3f5-8df1-f3a1-4eb18db89f48>
2012-01-28 21:15:07 +0000

    finish basic algorithm

--- NativeBayse/src/WordsData.java
+++ NativeBayse/src/WordsData.java
@@ -34,6 +34,53 @@ public class WordsData
 		
 	}
 	
+	public void AddArticle2Class(int classType,int articleIndex)
+	{
+		m_articleClass.add(articleIndex, classType);
+	}
+	
+	private List<Integer> GetWordInDocumnet(int articleIndex)
+	{
+		return m_word_article.get(articleIndex);
+	}
+	
+	private List<Integer> GetAllArticlesInClass(int classType)
+	{
+		List<Integer> articlesList = new Vector<Integer>();
+		int i =1;
+		for(Integer aClass :m_articleClass)
+		{
+			if (aClass == classType)
+			articlesList.add(i++);	
+		}
+		return articlesList;
+	}
+	
+	
+	
+	public int GetCountWordInClass(int wordIndex,int classType)
+	{
+		int count = 0;
+		for (Integer aIndex: GetAllArticlesInClass(classType))
+		{
+			if (GetWordInDocumnet(aIndex).contains((Integer)wordIndex)) count++;
+		}
+		return count;
+	}
+		
+	public int GetTotalWordSInClass(int classType ,  intWarp K )
+	{
+		int sum = 0;
+		int k = 0;
+		for(Integer aIndex : GetAllArticlesInClass(classType))
+		{
+			k++;
+			sum += GetWordInDocumnet(aIndex).size();
+		}
+		K.setM_Value(k);
+		return sum;
+	} 
+	
 	public int GetWordIndex(String word)
 	{
 		return (m_wordList.contains(word)) ?  m_wordList.indexOf(word)+1 : NOT_EXIST;
@@ -41,18 +88,20 @@ public class WordsData
 	
 	public void  AddWordToArticle(int articleIndex,int wordIndex)
 	{
+		int aIndex = articleIndex-1;
+		int wIndex = wordIndex-1;
 		List<Integer> tempWordList ;
 		try
 		{
-			tempWordList = m_word_article.get(articleIndex);
+			tempWordList = m_word_article.get(aIndex);
 		}
 		catch (IndexOutOfBoundsException  ex) 
 		{
 				tempWordList = new Vector<Integer>();
 				m_word_article.add(tempWordList);
 		}
-		tempWordList.add(wordIndex);
-		m_word_article.set(articleIndex,tempWordList);
+		tempWordList.add(wIndex);
+		m_word_article.set(aIndex,tempWordList);
 			
 	}
 }
