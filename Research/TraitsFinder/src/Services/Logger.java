c9f12411ac75ca9542b2f69ef79619d5401b3373
shapiraoz@gmail.com <shapiraoz@gmail.com@b4e4f271-b3f5-8df1-f3a1-4eb18db89f48>
2012-04-25 16:24:45 +0000

    add Logger

---
+++ Research/TraitsFinder/src/Services/Logger.java
@@ -0,0 +1,71 @@
+package Services;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Logger
+{
+	final static String LOG_NAME ="TraitsFinder.log";
+	
+	public static Logger GetLogger()
+	{
+		if (new File(LOG_NAME).exists())
+		{
+			File file = new File(LOG_NAME);
+			file.delete();
+		}
+		if (m_logger==null)
+		{
+			m_logger = new Logger(LOG_NAME);
+		}
+		return m_logger;
+				
+	}
+	
+	
+	
+	public static void Write(String module, String msg)
+	{
+		WriteStringToFile(String.format("-[%s]-:[%s]", module,msg),m_filePath);
+	}
+	
+	public static void WriteLine(String module, String msg)
+	{
+		WriteStringToFile(String.format("-[%s]-:[%s]\n", module,msg),m_filePath);
+	}
+	
+	private Logger(String filePath)
+	{
+		m_filePath = filePath;
+	}
+	
+	
+	static boolean WriteStringToFile(String str,String filePath )
+	{
+		BufferedWriter out = null;
+		try
+		{
+			  fstream = new FileWriter(filePath,true);
+			  out = new BufferedWriter(fstream);
+			  out.write(str);
+			  out.close();
+		
+		}
+		catch (Exception e)
+		{
+			if (out != null)
+			{
+				try { out.close();} catch (IOException e1){}
+				return false;
+			}
+		}
+		return true;
+			
+	}
+
+	private static FileWriter fstream =null;
+	private static Logger m_logger = null;
+	private static String m_filePath;
+}
