275c993cc6b034d36046534c284961654f8e67e9
shapiraoz@gmail.com <shapiraoz@gmail.com@b4e4f271-b3f5-8df1-f3a1-4eb18db89f48>
2012-12-22 15:31:30 +0000

    fix the weight bug!!!!

--- Research/TraitsFinder1.1/src/TratisFinderTests/UserElementsLinkTest.java
+++ Research/TraitsFinder1.1/src/TratisFinderTests/UserElementsLinkTest.java
@@ -6,12 +6,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import Core.CommonDef;
 import Core.Serialization.ESerializerType;
 import Core.Serialization.SerializerFactory;
 import Elements.SubjectElement;
 import Elements.UserElement;
+import Services.FileServices;
+import Services.Neo4J.Neo4JActivation;
 
-public class UserElementsLinkTest {
+public class UserElementsLinkTest extends test{
 
 	
 	UserElement user1 ;
@@ -21,18 +24,28 @@ public class UserElementsLinkTest {
 	@Before
 	public void setUp() throws Exception
 	{
+		super.setUp();
+		
+		if (FileServices.PathExist(USER_LINK_GRAPH_TEST)) FileServices.DeleteFolder(GetClassName(), USER_LINK_GRAPH_TEST);
+		CommonDef.GRAPH_DB_DIR = USER_LINK_GRAPH_TEST;
+		CommonDef.SET_GRAPH = true;
+		Neo4JActivation.Start(USER_LINK_GRAPH_TEST);
 		
 		SubjectElement sbElmShared1 = new SubjectElement("Pizza");
 		SubjectElement sbElmShared2 = new SubjectElement("cars");
+		
 		user1 = new UserElement("ben");
 		user2 = new UserElement("joe");
+		
 		user1.AddElement(sbElmShared1);
 		user1.AddElement(sbElmShared2);
 		user1.AddElement(new SubjectElement("race"));
 		user1.AddElement(new SubjectElement("java"));
+		
 		user2.AddElement(sbElmShared2);
 		user2.AddElement(sbElmShared1);
 		user2.AddElement(new SubjectElement("biff"));
+		
 		user1.SetSerializer(SerializerFactory.GetInstance().GetSerializer(ESerializerType.eNeo4J, user1, USER_LINK_GRAPH_TEST));
 		user2.SetSerializer(SerializerFactory.GetInstance().GetSerializer(ESerializerType.eNeo4J, user2, USER_LINK_GRAPH_TEST));
 		
@@ -45,8 +58,11 @@ public class UserElementsLinkTest {
 
 	@Test
 	public void testSerialize() {
-		fail("Not yet implemented");
+		user1.Serialize();
+		user2.Serialize();
+		WriteToConsole("check graph ...");
 	}
+	
 	/*
 	@Test
 	public void testLink() {
