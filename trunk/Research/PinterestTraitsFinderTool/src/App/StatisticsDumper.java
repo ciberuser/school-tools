e74e119749fb7cea305296c19724ca14e3dc9365
shapiraoz@gmail.com <shapiraoz@gmail.com@b4e4f271-b3f5-8df1-f3a1-4eb18db89f48>
2013-06-21 15:37:57 +0000

    version 1.1.5.1 : * add rollback process - for now tested on single process .- * add StatisticsDumper for all data - tested on 81 users - works!! * loss some wight challenge : -- cancel download for old file. -- will cancel adding to head element.

--- trunk/Research/PinterestTraitsFinderTool/src/App/StatisticsDumper.java
+++ trunk/Research/PinterestTraitsFinderTool/src/App/StatisticsDumper.java
@@ -9,18 +9,25 @@ import java.util.Map;
 import Services.CSVBuilder;
 import Services.Log.ELogLevel;
 import Core.CommonCBase;
+import Core.IStatisticsDumper;
 import Core.PinterestStatisticsBuilder;
 import Core.StatisticsBuilder;
 
-public class StatisticsDumper extends CommonCBase
+public class StatisticsDumper extends CommonCBase implements IStatisticsDumper
 {
-	enum EState {eUeser2Subject,eSubject2User,esubject2Item};
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	enum EState {eUeser2Subject,eSubject2User,esubject2Item,eAll};
 	
 	public StatisticsDumper(String csvRootFolderPath)
 	{
 		m_csvPoplarUserFilePath = csvRootFolderPath+ "/" +"users.csv" ;
 		m_csvPoplarSubjectsFilePath = csvRootFolderPath + "/"  + "subjects.csv";
 		m_csvItemCount = csvRootFolderPath + "/" + "items.csv"; 
+		m_cvsBigTable_file = csvRootFolderPath + "/" + "data.csv"; 
 	}
 	
 	public boolean DumpStatistics()
@@ -74,6 +81,9 @@ public class StatisticsDumper extends CommonCBase
 			header[0] = "Subject";
 			header[1] = "Number of Items";
 			break;
+		case eAll:
+			header[0] = "users";
+			header[1] = "subject";
 		default:
 			break;
 		}
@@ -81,6 +91,38 @@ public class StatisticsDumper extends CommonCBase
 		return header;
 	}
 	
+	public boolean DumpAllTargetsStatData()
+	{
+		boolean ret=true;
+		WriteLineToLog("going to write statictics file... filepath = "+ m_cvsBigTable_file, ELogLevel.INFORMATION);
+		
+		if(!DumpUsersData(StatisticsBuilder.GetInstance().GetStatistics(PinterestStatisticsBuilder.EPinStatsType.eUserSubject).getTargetToSubTargetDB(), m_cvsBigTable_file))
+		{
+			String errMsguser = "Failed to dump all users data !";
+			ret&=false;
+			WriteLineToLog(errMsguser, ELogLevel.ERROR);
+			WriteToConsole(errMsguser);
+		}
+		return ret;
+	}
+	
+	private boolean DumpUsersData(Map<String,List<String>> statDB,String filePath)
+	{
+		boolean res = true ;
+		CSVBuilder builer = new CSVBuilder(filePath);
+		res &=builer.WriteHeader(CreateCountHeader(EState.eAll));
+		Iterator<Map.Entry<String, List<String>>>  it = statDB.entrySet().iterator();
+		while(it.hasNext())
+		{
+			Map.Entry<String, List<String>> mapEntry = (Map.Entry<String, List<String>>) it.next();
+			mapEntry.getValue().add(0, mapEntry.getKey());
+			String[] line = mapEntry.getValue().toArray(new String[0]);
+			//(String.format("add the line{%s,%s} to statistics %s file ", filePath,line[0],line[1]), ELogLevel.INFORMATION);
+ 			res &= builer.WriteLine(line);
+		}
+		
+		return res;
+	}
 	
 	private boolean DumpCountStatistics(Map<String,List<String>> statDB, String filepath,EState state)
 	{
@@ -111,5 +153,6 @@ public class StatisticsDumper extends CommonCBase
 	private String m_csvPoplarUserFilePath;
 	
 	private String m_csvItemCount;
+	private String m_cvsBigTable_file;
 	
 }
