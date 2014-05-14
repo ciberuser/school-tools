f88de22c5069bde6552d33f585db2b3827bdf755
shapiraoz@gmail.com <shapiraoz@gmail.com@b4e4f271-b3f5-8df1-f3a1-4eb18db89f48>
2014-05-14 08:30:22 +0000

    add UserNearCoffeeShop and UserPhysicalLocation

---
+++ School/UserModel_HW/IBUMCORE/src/infobeadCollection/UserNearCoffeeShop.java
@@ -0,0 +1,26 @@
+package infobeadCollection;
+
+import genericInfoBead.InfoBead;
+import genericInfoBead.Triplet;
+
+public class UserNearCoffeeShop extends InfoBead {
+
+		
+	public UserNearCoffeeShop()
+	{
+		super();
+	}
+	
+	@Override
+	public void handleData(Triplet data) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
